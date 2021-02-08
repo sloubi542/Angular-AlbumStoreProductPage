0b1c5dfc5bbdf07080276ee724c17b93bcdef049
Guillaume Dutertre <dutertre.g@sfeir.com>
2021-02-08 14:30:32 +0100

    product service + test fixes
    map import and Album interface not yet created

---
+++ src/app/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl: string = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number): Observable<any> {
+    return this._http.get(this._albumUrl).pipe(map(response => response.json()));
+  }
+}
